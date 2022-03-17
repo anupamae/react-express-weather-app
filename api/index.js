1f8d6a1b223d8a25367a3245a22dcc859c58f03f
Anupama Ellath <anupamae8@gmail.com>
2022-03-17 18:31:05 +0100

    Replaced 'request' with 'node-fetch' package

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
-const request = require('request');
+const fetch = require('node-fetch');
 const bodyParser = require('body-parser');
 
 // NOTE: Create a file named .env and define OPEN_WEATHER_MAP_API_KEY variable
@@ -9,43 +9,39 @@ const bodyParser = require('body-parser');
 dotenv.config();
 
 const app = express();
+const port = 3001
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api/weather', (req, res) => {
-  request({
-    uri: 'http://api.openweathermap.org/data/2.5/weather',
-    qs: {
-      APPID: process.env.OPEN_WEATHER_MAP_API_KEY,
-      units: 'metric',
-      q: req.query.city
-    },
-    callback: (error, response, body) => {
-      if (!error && response.statusCode === 200) {
-        const bodyJson = JSON.parse(body);
-        res.json({
-          temp: bodyJson.main.temp.toFixed(0),
-          feelsLike: bodyJson.main.feels_like.toFixed(0),
-          minTemp: bodyJson.main.temp_min.toFixed(0),
-          maxTemp: bodyJson.main.temp_max.toFixed(0),
-          pressure: bodyJson.main.pressure,
-          humidity: bodyJson.main.humidity,
-          icon: `https://openweathermap.org/img/wn/${bodyJson.weather[0].icon}@4x.png`,
-          desc: bodyJson.weather[0].main
-        });
-      } else {
-        res.status(response.statusCode).json(error);
-      }
-    }
-  });
+app.get('/api/weather', async (req, res) => {
+
+  try {
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?units=metric&APPID=${process.env.OPEN_WEATHER_MAP_API_KEY}&q=${req.query.city}`);
+    const data = await response.json();
+    // console.log(data);
+    res.json({
+      temp: data.main.temp.toFixed(0),
+      feelsLike: data.main.feels_like.toFixed(0),
+      minTemp: data.main.temp_min.toFixed(0),
+      maxTemp: data.main.temp_max.toFixed(0),
+      pressure: data.main.pressure,
+      humidity: data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`,
+      desc: data.weather[0].main
+    });
+  } catch (ex) {
+    res.status(500).json({
+      message: ex.message
+    });
+  }
 });
 
 module.exports.app = app;
 
 // See http://www.marcusoft.net/2015/10/eaddrinuse-when-watching-tests-with-mocha-and-supertest.html
 if (!module.parent) {
-  app.listen(3001);
+  app.listen(port, ()=> console.log(`Server is listening on ${port}`))
 }
